b88c46f6ddde3f5bb6b6d4e4890e9791c9ba1497
Adedoyin <isradeoluwa@gmail.com>
2021-12-10 01:51:21 +0100

    THEME: Screen background decoration completed

---
+++ lib/themes.dart/bg_decoration.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = BoxDecoration(
+  gradient: LinearGradient(
+    colors: [
+      Color.fromRGBO(51, 51, 51, 1),
+      Color.fromRGBO(85, 93, 80, 1),
+    ],
+    begin: Alignment(-1, -1),
+    end: Alignment(1, 1),
+  ),
+);
