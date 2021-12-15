ba41f541e3da4eb99a802456e08438938f4922ce
Adedoyin <isradeoluwa@gmail.com>
2021-12-15 01:01:45 +0100

    Selected bottom nav theme done

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -4,20 +4,58 @@ import 'package:movie_ticket_app/state_manager/states.dart';
 import 'package:movie_ticket_app/themes.dart/colors.dart';
 import 'package:get/get.dart';
 
+List<Icon> iconList = const [
+  Icon(
+    FontAwesomeIcons.home,
+  ),
+  Icon(
+    FontAwesomeIcons.ticketAlt,
+  ),
+  Icon(
+    FontAwesomeIcons.user,
+  ),
+];
+
 bottomNavBar() => Obx(() => BottomNavigationBar(
       backgroundColor: backgroundColor,
       selectedItemColor: white,
       unselectedItemColor: white.withOpacity(0.3),
       iconSize: 18,
       onTap: (currentIndex) {
-        print(currentIndex);
         homeState.currentTab.value = currentIndex;
       },
       currentIndex: homeState.currentTab.value,
-      items: const [
+      items: [
         BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.home,
+            icon: ClipOval(
+              child: Container(
+                width: 50,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  white.withOpacity(0.1),
+                  white.withOpacity(0.5)
+                ])),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      FontAwesomeIcons.home,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    ClipOval(
+                        child: Container(
+                      alignment: Alignment.center,
+                      height: 5,
+                      width: 5,
+                      color: white,
+                    ))
+                  ],
+                ),
+              ),
             ),
             label: ''),
         BottomNavigationBarItem(
