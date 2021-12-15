f4900888269b32d41fc911bae0295f48297acf32
Adedoyin <isradeoluwa@gmail.com>
2021-12-15 01:35:00 +0100

    Bottom Nav completed

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -4,6 +4,7 @@ import 'package:movie_ticket_app/state_manager/states.dart';
 import 'package:movie_ticket_app/themes.dart/colors.dart';
 import 'package:get/get.dart';
 
+// Bottom Nav Icons
 List<Icon> iconList = const [
   Icon(
     FontAwesomeIcons.home,
@@ -16,6 +17,41 @@ List<Icon> iconList = const [
   ),
 ];
 
+// Bottom Navigation Bar Item
+BottomNavigationBarItem bottomNavItem(int index) => BottomNavigationBarItem(
+    icon: index != homeState.currentTab.value
+        ? iconList[index]
+        : ClipOval(
+            child: Container(
+              width: 55,
+              height: 55,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                white.withOpacity(0.1),
+                white.withOpacity(0.5)
+              ])),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  iconList[index],
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  ClipOval(
+                      child: Container(
+                    alignment: Alignment.center,
+                    height: 5,
+                    width: 5,
+                    color: white,
+                  ))
+                ],
+              ),
+            ),
+          ),
+    label: '');
+
+// Bottom Navigation Bar
 bottomNavBar() => Obx(() => BottomNavigationBar(
       backgroundColor: backgroundColor,
       selectedItemColor: white,
@@ -26,47 +62,6 @@ bottomNavBar() => Obx(() => BottomNavigationBar(
       },
       currentIndex: homeState.currentTab.value,
       items: [
-        BottomNavigationBarItem(
-            icon: ClipOval(
-              child: Container(
-                width: 50,
-                height: 50,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                  white.withOpacity(0.1),
-                  white.withOpacity(0.5)
-                ])),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      FontAwesomeIcons.home,
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    ClipOval(
-                        child: Container(
-                      alignment: Alignment.center,
-                      height: 5,
-                      width: 5,
-                      color: white,
-                    ))
-                  ],
-                ),
-              ),
-            ),
-            label: ''),
-        BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.ticketAlt,
-            ),
-            label: ''),
-        BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.user,
-            ),
-            label: ''),
+        ...List.generate(iconList.length, (index) => bottomNavItem(index))
       ],
     ));
