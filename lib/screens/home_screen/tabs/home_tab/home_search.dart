8a9d13203f8ea027e90c0c58f7626af745fe97be
Adedoyin <isradeoluwa@gmail.com>
2021-12-17 23:39:50 +0100

    Home Search completed

--- lib/screens/home_screen/tabs/home_tab/home_search.dart
+++ lib/screens/home_screen/tabs/home_tab/home_search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movie_ticket_app/themes.dart/colors.dart';
 
 class HomeSearch extends StatelessWidget {
@@ -13,11 +14,42 @@ class HomeSearch extends StatelessWidget {
           return Container(
             margin: const EdgeInsets.only(left: 25, right: 25, bottom: 10),
             child: TextField(
+              style: const TextStyle(color: white),
+              cursorColor: white.withOpacity(0.7),
               decoration: InputDecoration(
                 filled: true,
                 fillColor: white.withOpacity(val),
+                prefixIcon: Icon(
+                  FontAwesomeIcons.search,
+                  color: white.withOpacity(0.7),
+                  size: 18,
+                ),
+                suffixIcon: SizedBox(
+                  width: 30,
+                  child: Row(
+                    children: [
+                      Text(
+                        '|',
+                        style: TextStyle(
+                            color: white.withOpacity(0.1), fontSize: 25),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.mic,
+                        color: white.withOpacity(0.5),
+                      )
+                    ],
+                  ),
+                ),
+                hintText: 'Search',
+                hintStyle: TextStyle(
+                    color: white.withOpacity(0.4),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
                 border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(val * 48),
+                    borderRadius: BorderRadius.circular(val * 55),
                     borderSide: BorderSide.none),
               ),
             ),
