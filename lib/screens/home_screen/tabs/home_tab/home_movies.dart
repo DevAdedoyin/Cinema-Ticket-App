13d25a4f4861326a6052854e3b5cec22a0b8786d
Adedoyin <isradeoluwa@gmail.com>
2021-12-19 21:57:14 +0100

    IMDB RATING

--- lib/screens/home_screen/tabs/home_tab/home_movies.dart
+++ lib/screens/home_screen/tabs/home_tab/home_movies.dart
@@ -71,13 +71,16 @@ class _HomeMoviesState extends State<HomeMovies> {
         margin: const EdgeInsets.symmetric(horizontal: 10),
         height: 380,
         width: 270,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: white),
         child: Stack(children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Image.asset(
               img,
               fit: BoxFit.cover,
+              height: 380,
+              width: 270,
             ),
           ),
           Positioned(
@@ -85,18 +88,42 @@ class _HomeMoviesState extends State<HomeMovies> {
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Icon(
-                      Icons.star,
-                      color: yellow,
-                      size: 20,
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(
+                          Icons.star,
+                          color: yellow,
+                          size: 20,
+                        ),
+                        Text(
+                          ratingNum,
+                          style: const TextStyle(
+                              color: gold,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
-                    Text(
-                      ratingNum,
-                      style: const TextStyle(
-                          color: gold,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+                    const SizedBox(
+                      width: 130,
+                    ),
+                    Container(
+                      width: 65,
+                      height: 23,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: gold,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Text(
+                        'IMDB 7.5',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: backgroundColor),
+                      ),
                     )
                   ],
                 ),
