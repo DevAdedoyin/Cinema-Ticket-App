c9beaf195fbd495c1b99a5f14608f442d9c41dfb
Adedoyin <isradeoluwa@gmail.com>
2021-12-22 01:02:10 +0100

    Date card for movies started

--- lib/screens/movie_detail/movie_detail.dart
+++ lib/screens/movie_detail/movie_detail.dart
@@ -1,42 +1,161 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movie_ticket_app/models/movie_model.dart';
 import 'package:movie_ticket_app/themes.dart/colors.dart';
 import 'package:movie_ticket_app/widgets/ratings_and_imdb.dart';
 
-class MovieDetail extends StatelessWidget {
+class MovieDetail extends StatefulWidget {
   final int index;
   const MovieDetail(this.index, {Key? key}) : super(key: key);
 
   @override
+  State<MovieDetail> createState() => _MovieDetailState();
+}
+
+class _MovieDetailState extends State<MovieDetail> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 380,
-            child: Stack(
-              children: [
-                SizedBox(
-                  width: double.maxFinite,
-                  child: Hero(
-                    tag: index,
-                    child: Image.asset(
-                      movieDataList[index].image!,
-                      fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 380,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    width: double.maxFinite,
+                    child: Hero(
+                      tag: widget.index,
+                      child: Image.asset(
+                        movieDataList[widget.index].image!,
+                        fit: BoxFit.fill,
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
+                  Positioned(
                     bottom: 5,
-                    left: 5,
+                    left: 25,
                     child: ratingsAndImdb(
-                      10,
-                      movieDataList[index].ratingNumber!,
-                    )),
-              ],
+                      15,
+                      movieDataList[widget.index].ratingNumber!,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 10,
+            ),
+            ListTile(
+              dense: true,
+              leading: Icon(
+                FontAwesomeIcons.directions,
+                color: white.withOpacity(0.5),
+                size: 17,
+              ),
+              title: Text(
+                'Director',
+                style: TextStyle(
+                    color: white.withOpacity(0.7),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                movieDataList[widget.index].directorName!,
+                style: TextStyle(
+                  color: white.withOpacity(0.5),
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            ListTile(
+              dense: true,
+              leading: Icon(
+                FontAwesomeIcons.pen,
+                size: 17,
+                color: white.withOpacity(0.5),
+              ),
+              title: Text(
+                'Writer(s)',
+                style: TextStyle(
+                    color: white.withOpacity(0.7),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                movieDataList[widget.index].writerName!,
+                style: TextStyle(
+                  color: white.withOpacity(0.5),
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: 320,
+              child: Text(
+                movieDataList[widget.index].description!,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    height: 1.5,
+                    color: white.withOpacity(0.5),
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(
+                  left: 20, right: 20, top: 15, bottom: 10),
+              child: Text(
+                'Select Date',
+                style: TextStyle(
+                    color: white.withOpacity(0.7),
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 80,
+              child: ListView.builder(
+                itemCount: movieDataList.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 7),
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                            color: white.withOpacity(0.5), width: 0.5),
+                      ),
+                      height: 60,
+                      width: 50,
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                              color: gold,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                shrinkWrap: true,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
