d8856c47902f18d106dd8a5e3c475f40e2ab6ebb
Adedoyin <isradeoluwa@gmail.com>
2021-12-25 12:29:51 +0100

    Ticket tab header

--- lib/screens/home_screen/tabs/ticket_tab.dart
+++ lib/screens/home_screen/tabs/ticket_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_ticket_app/themes.dart/colors.dart';
 
 class TicketTab extends StatefulWidget {
   const TicketTab({Key? key}) : super(key: key);
@@ -10,9 +11,28 @@ class TicketTab extends StatefulWidget {
 class _TicketTabState extends State<TicketTab> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [Text('Tickets')],
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'My tickets',
+                  style: TextStyle(
+                      color: white, fontSize: 23, fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.pending_outlined,
+                  color: white,
+                  size: 24,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
