42269c39f26c707dba49e19c1316ca7adfef0113
Adedoyin <isradeoluwa@gmail.com>
2022-01-31 11:45:09 +0100

    Debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Movie Ticket',
       theme: ThemeData(
           fontFamily: 'Oxygen',
